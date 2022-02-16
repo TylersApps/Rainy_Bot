898e492583a72508aca859eb93f1c922366ce713
tholley7 <tholley7@uncc.edu>
2022-02-16 17:37:48 -0500

    Add /help command

--- bot/rainy_bot.py
+++ bot/rainy_bot.py
@@ -1,27 +1,35 @@
-import colorama
+from colorama import Fore, Style, init
 import config
-import disnake
-from disnake.ext import commands
+import nextcord
+from nextcord import Interaction
+from nextcord.ext import commands
 
 
-client = disnake.Client()
+# Initialize console colors
+CY = Fore.CYAN
+GR = Fore.GREEN
+BLU = Fore.BLUE
+RES = Style.RESET_ALL
+
+
 bot = commands.Bot(
-    command_prefix='!',
-    test_guilds=[123456789], # Optional
-    sync_commands_debug=True
+    command_prefix="!",
+    intents=nextcord.Intents.all()
 )
 
+test_guild_id = config.cfg['guild_id']
 
-@client.event
+
+# Sends a message to console when bot is online in server(s).
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'\n{GR}Logged in as {bot.user}{RES}\n')
+
+# Send a list of available commands to the channel where this command is sent.
+@bot.slash_command(guild_ids=[test_guild_id])
+async def help(interaction: Interaction):
+    print(f'{BLU}Help command used!{RES}')
+    await interaction.response.send_message('How can I help?')
 
 
-# NEEDS FURTHER TESTING
-@bot.slash_command(description='Responds with list of available commands.')
-async def help(inter):
-    await inter.response.send_message("Test successful!")
-
-
-
-client.run(config.cfg['token'])
+bot.run(config.cfg['token'])
