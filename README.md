7756dcadd8454c63e603fc3ca5b97f6abb0dc891
tholley7 <tholley7@uncc.edu>
2022-02-17 19:57:54 -0500

    Add /roll command

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
 # Rainy Bot
 Rainy Bot is a multi-use Discord bot that I use in my servers. It has many functions (outlined below) and I would love to try new functions so pop an idea in the [issues](https://github.com/tholley7/Rainy_Bot/issues) tab if you have an idea and I will take a look!
 
+
 ## Commands
 
 ### Current
 - `/help` displays a list of all available commands (different list for admins and non-admins).
 - `/randompost <subreddit_name>` gets a random post from the specified subreddit.
+- `/roll <(x)d(y)>` rolls x dice with y sides (e.g. "/roll 4d6" rolls 4 6-sded dice)
 
 ### Planned
 - `/define <word>` defines the word passed in using Merriam-Webster's [dictionary API](https://dictionaryapi.com/)
 - `/menu <type>` sends the specified menu type to the channel where the command was sent.
-- `/roll <(x)D(y)>` rolls x dice with y sides (e.g. "/roll 4d6" rolls 4 6-sded dice)
+
 
 ## Built With
 - [Nextcord](https://nextcord.readthedocs.io/) - Discord API Wrapper
 - [Async PRAW](https://asyncpraw.readthedocs.io/en/stable/code_overview/models/subreddit.html) - Asynchronous Reddit API Wrapper
 - [Colorama](https://pypi.org/project/colorama/) - Colorful console outputs
 
+
 ## Contributors
 - Tyler Holley
 
+
 ## License
 This project is licensed under the **MIT License** - see the [LICENSE.md](LICENSE.md) file for details.
+
 
 ## Acknowledgements
 Inspired by:
--- bot/rainy_bot.py
+++ bot/rainy_bot.py
@@ -4,9 +4,9 @@ from nextcord.ext import commands
 
 from colorama import Fore, Style, init
 import asyncpraw
-import random
 import json
 import aiohttp, asyncio
+from random import randint
 
 from config import reddit, cfg
 
@@ -16,6 +16,7 @@ CY = Fore.CYAN
 GR = Fore.GREEN
 BLU = Fore.BLUE
 RED = Fore.RED
+YLW = Fore.YELLOW
 RES = Style.RESET_ALL
 
 # Initialize bot
@@ -23,6 +24,7 @@ bot = commands.Bot(command_prefix="!", intents=nextcord.Intents.all() )
 
 test_guild_id = cfg['guild_id']
 embed_color = nextcord.Colour.from_rgb(47, 49, 54)
+error_color = nextcord.Colour.from_rgb(217, 95, 87)
 
 # Sends a message to console when bot is online in server(s).
 @bot.event
@@ -39,7 +41,8 @@ async def help(interaction: Interaction):
         title='Rainy Bot Commands',
         description='\
             `/help` displays a list of all available commands\n\
-            `/randompost <subreddit_name>` gets a random post from a specific subreddit.',
+            `/randompost <subreddit_name>` gets a random post from a specific subreddit.\n\
+            `/roll <(x)d(y)>` rolls x dice with y sides (e.g. "/roll 4d6" rolls 4 6-sded dice)',
         color=embed_color
     )
 
@@ -61,15 +64,18 @@ async def randompost(interaction: Interaction, subreddit_name: str = SlashOption
         subreddit = await reddit.subreddit(subreddit_name, fetch=True)
     except Exception: 
         embed = nextcord.Embed(
+            title='Invalid input',
             description=f"r/{subreddit_name} doesn't exist.",
-            color=nextcord.Colour.from_rgb(217, 95, 87)
+            color=error_color
         )
         await interaction.followup.send(embed=embed)
+        print(f'{RED}Invalid subreddit input{RES}')
         return
 
     # Initialize submission and url variables
     submission = await subreddit.random()
     url = submission.url
+    permalink = f'https://www.reddit.com{submission.permalink}'
 
     # Initialize title and shorten to max 253 characters
     title = submission.title
@@ -80,15 +86,12 @@ async def randompost(interaction: Interaction, subreddit_name: str = SlashOption
     post_body = submission.selftext
     if len(post_body) > 500:
         post_body = post_body[:497]
-        post_body += " ..."
-
-    # Send confirmation message
-    print(f'Sending the following post from {CY}{submission.subreddit}{RES}: {submission.title}') 
+        post_body += " ..." 
 
     # Create embed
     embed = nextcord.Embed(
         title=title,
-        url=url,
+        url=permalink,
         description=post_body,
         color=embed_color
     )
@@ -98,7 +101,83 @@ async def randompost(interaction: Interaction, subreddit_name: str = SlashOption
         embed.set_image(url=url)
 
     # Send embed to channel
-    await interaction.followup.send(embed=embed)
+    await interaction.followup.send(embed=embed) 
+
+    # Send confirmation message
+    print(f'Sent post from {YLW}{submission.subreddit}{RES}: {submission.title}')
+
+
+# Send the definition of the specified word
+@bot.slash_command(guild_ids=[test_guild_id], description='Get the definition of a word')
+async def define(interaction: Interaction, word: str = SlashOption(description="Word to define")):
+    print(f'{CY}Define{RES} command used!')
+
+    api_url = f"http://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+
+    embed = nextcord.Embed(
+        color=embed_color,
+        title=f'Definition of "{word.lower()}":',
+    )
+
+    # Get the json data for the specified word
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url, ssl=False) as resp:
+                data = await resp.json()
+    except Exception as ex: 
+        print(f'{RED}[ERROR]: {ex}{RES}')
+        return
+    
+    content = data[0]['meanings']
+
+    for item in content:
+        print(item['definitions'][0])
+
+
+    await interaction.response.send_message(embed=embed)
+    
+    # Send confirmation message
+    print(f'Sent definition of {YLW}{word}{RES}!')
+
+
+# Roll die or dice based on input
+@bot.slash_command(guild_ids=[test_guild_id], description='Roll a specific amount of dice with a specific amount of sides.')
+async def roll(interaction: Interaction, dice: str=SlashOption(description="Specified dice")):
+    print(f'{CY}Roll{RES} command used!')
+
+    # Initialize function variables
+    dice = dice.lower()
+    dice_split = dice.split('d')
+    total = 0
+
+    # Initialize embed with default embed color
+    embed = nextcord.Embed(color=embed_color)
+
+    # Check for invalid input
+    if len(dice_split) != 2 or dice.isalpha() or dice.isnumeric():
+        embed.title='Invalid input'
+        embed.description='`dice` input should be formatted like **2D20** or **1D4**'
+        embed.color=error_color
+
+        print(f'{RED}Invalid dice input{RES}')
+    else:
+        num_rolls = int(dice_split[0])
+        num_sides = int(dice_split[1])
+
+        # Roll dice and get total of all rolls
+        for x in range(num_rolls):
+            total += randint(0, num_sides)
+
+        # Update embed with dice and total
+        embed.title = f'Rolled {dice}'
+        embed.description = f'Outcome: **{total}**'
+
+        print(f'Rolled {YLW}{dice}{RES} for a total of {YLW}{total}{RES}!')
+
+
+    await interaction.response.send_message(embed=embed)
+    
+
 
 
 bot.run(cfg['token'])
